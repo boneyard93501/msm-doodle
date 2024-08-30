59058f63870758d2b57d7b6eaf7cf72fc146b3bd
boneyard93501 <4523011+boneyard93501@users.noreply.github.com.>
2024-08-30 15:41:03 -0500

    update with msm

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
+use curve25519_dalek::constants;
 use curve25519_dalek::ristretto::RistrettoPoint;
+use curve25519_dalek::traits::MultiscalarMul;
 use curve25519_dalek::scalar::Scalar;
 use sha3::Sha3_512;
 use rand::rngs::OsRng;
@@ -10,32 +11,57 @@ fn hash_to_ristretto_point(data: &str) -> RistrettoPoint {
 }
 
 
-fn pedersen_commitment(value: Scalar, blinding: Scalar) -> RistrettoPoint {
+fn pedersen_commitment(value: Scalar, blinding: Scalar, rp: RistrettoPoint) -> RistrettoPoint {
     let h = hash_to_ristretto_point("H");
-    value * RISTRETTO_BASEPOINT_POINT + blinding * h
+    value * rp + blinding * h
 }
 
 fn verify_pedersen_commitment(
     commitment: RistrettoPoint,
     value: Scalar,
-    blinding: Scalar
+    blinding: Scalar,
+    rp: RistrettoPoint
 ) -> bool {
     let h = hash_to_ristretto_point("H");
-    let expected_commitment = value * RISTRETTO_BASEPOINT_POINT + blinding * h;
+    let expected_commitment = value * rp + blinding * h;
     commitment == expected_commitment
 }
 
 fn main() {
+
+    // https://doc.dalek.rs/curve25519_dalek/traits/trait.MultiscalarMul.html
+
+    let a = Scalar::from(87329482u64);
+    let b = Scalar::from(37264829u64);
+    let c = Scalar::from(98098098u64);
+
+    // Some points
+    let P = constants::RISTRETTO_BASEPOINT_POINT;
+    let Q = P + P;
+    let R = P + Q;
+
+    // A1 = a*P + b*Q + c*R
+    let abc = [a,b,c];
+    let A1 = RistrettoPoint::multiscalar_mul(&abc, &[P,Q,R]);
+
+    // Note: (&abc).into_iter(): Iterator<Item=&Scalar>
+
+    // A2 = (-a)*P + (-b)*Q + (-c)*R
+    let minus_abc = abc.iter().map(|x| -x);
+    let A2 = RistrettoPoint::multiscalar_mul(minus_abc, &[P,Q,R]);
+    // Note: minus_abc.into_iter(): Iterator<Item=Scalar>
+
+    assert_eq!(A1.compress(), (-A2).compress());
 
     let seed_value = 2024u64;
     let value = Scalar::from(seed_value);
     let blinding = Scalar::random(&mut OsRng);
 
     // Generate the commitment
-    let commitment = pedersen_commitment(value, blinding);
+    let commitment = pedersen_commitment(value, blinding, A1);
     println!("Pedersen Commitment: {:?}", commitment);
 
     // Verify the commitment
-    let is_valid = verify_pedersen_commitment(commitment, value, blinding);
+    let is_valid = verify_pedersen_commitment(commitment, value, blinding, A1);
     println!("Is the commitment valid? {}", is_valid);
 }
